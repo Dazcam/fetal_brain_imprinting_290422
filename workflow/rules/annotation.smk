ebf5a859ee0ed96dbd5a5809e5293e88215365b7
Dazcam <camerond@cardiff.ac.uk>
2022-12-05 16:40:20 +0000

    Separate ref and map snp 2 gene steps

--- workflow/rules/annotation.smk
+++ workflow/rules/annotation.smk
@@ -1,11 +1,40 @@
-rule ase_map_snps_to_genes:
-    input:  genes = "../resources/sheets/Tucci_2019_mmc1.xlsx"
+rule ase_get_SNP_refs:
+    output: "../results/13GETREFS/homo_sapiens-chr{CHR}.vcf.gz",
+    params: OUT_DIR = "../results/13GETREFS/",
+            REPO_SNPS_DIR = "https://ftp.ensembl.org/pub/release-108/variation/vcf/homo_sapiens/",
+            SNPS = "homo_sapiens-chr{CHR}.vcf.gz",
+    log:    "../results/00LOG/13GETREFS/get_SNP_refs_{CHR}.log"
+    shell:
+            "wget -nc {params.REPO_SNPS_DIR}{params.SNPS} -P {params.OUT_DIR} &> {log}"
+
+rule ase_get_GENE_refs:
+    output: "../results/13GETREFS/Homo_sapiens.GRCh38.108.chromosome.{CHR}.gff3.gz",
+    params: OUT_DIR = "../results/13GETREFS/",
+            REPO_GENES_DIR = "https://ftp.ensembl.org/pub/release-108/gff3/homo_sapiens/",
+            GENES = "Homo_sapiens.GRCh38.108.chromosome.{CHR}.gff3.gz"
+    log:    "../results/00LOG/13GETREFS/get_GENE_refs_{CHR}.log"
+    shell:
+            "wget -nc {params.REPO_GENES_DIR}{params.GENES} -P {params.OUT_DIR} &> {log}"
+
+rule ase_map_snps_to_genes_genomewide:
+    input:  "../results/13GETREFS/homo_sapiens-chr{CHR}.vcf.gz",
+            "../results/13GETREFS/Homo_sapiens.GRCh38.108.chromosome.{CHR}.gff3.gz"
+    output: "../results/14SNP2GENES_GW/chr{CHR}_genes_no_snps.txt"
+    params: OUT_DIR = "../results/14SNP2GENES_GW/",
+            REF_DIR = "../results/13GETREFS/"
+    envmodules: "bcftools"
+    log:    "../results/00LOG/14SNP2GENES_GW/snps2genes_gw_chr{CHR}.log"
+    shell:
+            "scripts/imprinting_map_snps_2_genes.sh {wildcards.CHR} {params.OUT_DIR} {params.REF_DIR} &> {log}"
+
+#rule ase_map_snps_to_genes:
+#    input:  genes = "../resources/sheets/Tucci_2019_mmc1.xlsx"
 #           ase = "../results/11ASE_MAPPINGS/{sampleID}.ase.txt"
-    output: "../results/12SNP2GENES/snps2genes_maf_0.05.log"
-    params: "../results/12SNP2GENES/"
-    envmodules: "libgit2/1.1.0"
-    script:
-            "../scripts/imprinting_map_snps_2_genes.R"
+#    output: "../results/12SNP2GENES/snps2genes_maf_0.05.log"
+#    params: "../results/12SNP2GENES/"
+#    envmodules: "libgit2/1.1.0"
+#    script:
+#            "../scripts/imprinting_map_snps_2_genes.R"
 
 rule ase_cross_ref:
     input:  "../results/12SNP2GENES/snps2genes_maf_0.05.log"
---
+++ workflow/scripts/imprinting_map_snps_2_genes.sh
@@ -0,0 +1,85 @@
+CHR=$1
+OUT_DIR=$2
+
+REF_DIR=$3
+
+mkdir -p ${OUT_DIR}genes/
+
+printf '%s\n' '' '|  ----------------------------------------------  |'
+printf '%s\n' '|  MAPPING SNPS (MAF >= 0.05 TO GENES GENOME WIDE  |' ''
+printf '%s\n' '' '|  ----------------------------------------------  |'
+
+# Extract all snps with MAF >= 0.05
+printf '%s\n' '' 'Extracting all snps with MAF >= 0.05 (using bcftools) ...' 
+printf '%s\n' "In file: ${REF_DIR}homo_sapiens-chr${CHR}.vcf.gz ..." 
+printf '%s\n' "Out file: ${OUT_DIR}homo_sapiens-chr${CHR}_MAF.05.vcf.gz ..." ""
+
+#these are loaded as modules by snakemake
+#module load htslib/1.9
+#module load bcftools/1.16.0
+
+bcftools view -i 'MAF[0]>=0.05' ${REF_DIR}homo_sapiens-chr${CHR}.vcf.gz -o ${REF_DIR}homo_sapiens-chr${CHR}_MAF.05.vcf.gz
+
+# Extract all gene coords from gff3 and convert to bed (using bed ops)
+printf '%s\n' '' 'Extracting all gene coords from gff3 and convert to bed ...' ''
+zcat ${REF_DIR}Homo_sapiens.GRCh38.108.chromosome.${CHR}.gff3.gz |\
+awk '$3 == "gene"' |\
+grep Name |\
+gff2bed | awk -F'[;]' '{print $1 "\t" $2}' |\
+cut -f 1-3,11 |\
+sed 's/Name=//g' |\
+sed 's/^/chr/g' > ${OUT_DIR}chr${CHR}_genes.txt
+
+# Collect gene coords with no HGNC ID from gff3 and convert to bed (using bed ops)
+printf '%s\n' '' 'Collect gene coords with no HGNC ID from gff3 and convert to bed ...' ''
+zcat ${REF_DIR}Homo_sapiens.GRCh38.108.chromosome.${CHR}.gff3.gz |\
+awk '$3 == "gene"' |\
+grep -v Name |\
+gff2bed | cut -f 1-4 |\
+sed 's/Name=//g' > ${OUT_DIR}chr${CHR}_genes_noHGNC.txt
+
+# Convert SNPs into bed format (using bed ops)
+printf '%s\n' '' 'Convert SNPs into bed format ...' ''
+zcat ${REF_DIR}homo_sapiens-chr${CHR}_MAF.05.vcf.gz |\
+vcf2bed | cut -f 1-4 |\
+sed 's/^/chr/g' > ${OUT_DIR}chr${CHR}_snps_MAF.05.txt
+
+# Loop through genes and pull out all SNPs MAF >= 0.05
+printf '%s\n' '' "Loop through chr${CHR} genes and pull out all SNPs MAF >= 0.05 ..." ''
+while read -r line; 
+do 
+   echo $line > ${OUT_DIR}temp_${CHR}
+   GENE=$(echo $line | cut -d' ' -f4)
+   echo $GENE
+   bedops --element-of ${OUT_DIR}chr${CHR}_snps_MAF.05.txt ${OUT_DIR}temp_${CHR} > ${OUT_DIR}genes/${GENE}_snps;
+done < ${OUT_DIR}chr${CHR}_genes.txt
+
+rm ${OUT_DIR}temp_${CHR}
+
+# Reporting
+printf '%s\n' '' 'Prepping report files and info  ...' ''
+VARIANTS=$(zcat ${REF_DIR}homo_sapiens-chr${CHR}.vcf.gz | wc -l)
+VARIANTS_05=$(zcat ${REF_DIR}homo_sapiens-chr${CHR}_MAF.05.vcf.gz | wc -l)
+GENES=$(zcat ${REF_DIR}Homo_sapiens.GRCh38.108.chromosome.${CHR}.gff3.gz | awk '$3 == "gene"' | wc -l)
+GENES_HGNC=$(zcat ${REF_DIR}Homo_sapiens.GRCh38.108.chromosome.${CHR}.gff3.gz | awk '$3 == "gene"' | grep Name | wc -l)
+GENES_NO_HGNC=$(zcat ${REF_DIR}Homo_sapiens.GRCh38.108.chromosome.${CHR}.gff3.gz | awk '$3 == "gene"' | grep -v Name | wc -l)
+GENES_PROCESSED=$(ls ${OUT_DIR}genes/ | wc -l)
+GENES_NO_SNPS=$(wc -l ${OUT_DIR}genes/* | grep -w 0 | wc -l)
+
+wc -l ${OUT_DIR}genes/* > ${OUT_DIR}chr${CHR}_snp_cnts_per_gene.txt
+wc -l ${OUT_DIR}genes/* | grep -w 0 | cut -d/ -f3 | cut -d_ -f1 > ${OUT_DIR}chr${CHR}_genes_no_snps.txt
+
+printf '%s\n' '' "----------- Chr${CHR} Report -----------" ''
+printf '%s\n' "Total variants: $VARIANTS" 
+printf '%s\n' "Total variants MAF >= 0.05: $VARIANTS_05" 
+printf '%s\n' "Total genes: $GENES" 
+printf '%s\n' "Total HGNC genes: $GENES_HGNC" 
+printf '%s\n' "Total genes with no HGNC ID: $GENES_NO_HGNC" 
+printf '%s\n' "Total genes processed: $GENES_PROCESSED" 
+printf '%s\n' "Total genes containing no variants MAF >= 0.05: $GENES_NO_SNPS" ''
+printf '%s\n' "------------------------------------" ''
+
+
+printf '%s\n' '' '|  ----------------------------------------------  |'
+printf '%s\n' '|  ---------------  JOB FINISHED  ---------------  |'
+printf '%s\n' '|  ----------------------------------------------  |'
